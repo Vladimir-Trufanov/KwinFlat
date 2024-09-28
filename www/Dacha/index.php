2977f8c500b344c546fb3cf2b420e02a468c98f9
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2024-09-28 22:31:19 +0300

    Перенастроены лампочки

--- www/Dacha/index.php
+++ www/Dacha/index.php
@@ -53,16 +53,18 @@ try
       <h1>ESP32 - Дачная страница</h1>
    
       <?php
-      // echo 'parm='; echo $parm;
-      if ($parm=="led1on")
-         echo '<p>Состояние LED1: ВКЛ. </p><a id="LED1on"  class="button button-off" href="?Com=led1off">ВЫКЛ.</a>';
+      // Если поступила команда "включить вспышку" 
+      if ($parm=="led4on")
+         echo '<p>LED4 (вспышка) включена</p><a id="LED4on"  class="button button-on"  href="?Com=led4off">ВЫКЛ.</a>';
+      // "Выключить вспышку" (или по умолчанию)
       else
-         echo '<p>Состояние LED1: ВЫКЛ.</p><a id="LED1off" class="button button-on"  href="?Com=led1on"> ВКЛ. </a>';
-
-      if ($parm=="led2on")
-         echo '<p>Состояние LED2: ВКЛ. </p><a id="LED2on"  class="button button-off" href="?Com=led2off">ВЫКЛ.</a>';
+         echo '<p>LED4 (вспышка) выключена</p><a id="LED4off" class="button button-off" href="?Com=led4on"> ВКЛ. </a>';
+      // Если поступила команда "включить контрольный светодиод" 
+      if ($parm=="led33on")
+         echo '<p>LED33 - светодиод включен</p><a id="LED33on"  class="button button-on"  href="?Com=led33off">ВЫКЛ.</a>';
+      // "Выключить контрольный светодиод" (или по умолчанию)
       else
-         echo '<p>Состояние LED2: ВЫКЛ.</p><a id="LED2off" class="button button-on"  href="?Com=led2on"> ВКЛ. </a>';
+         echo '<p>LED33 - светодиод выключен</p><a id="LED33off" class="button button-off" href="?Com=led33on"> ВКЛ. </a>';
          
       echo "Всем большой привет с дачной страницы";
       ?>
@@ -73,28 +75,15 @@ try
       </footer>
       
       <?php
-      $dom = new DOMDocument('1.0','UTF-8');
-      $dom->formatOutput = true;
 
-      $root = $dom->createElement('student');
-      $dom->appendChild($root);
-
-      $result = $dom->createElement('result');
-      $root->appendChild($result);
-
-      $result->setAttribute('id', 1);
-      $result->appendChild( $dom->createElement('name', 'Opal Kole') );
-      $result->appendChild( $dom->createElement('sgpa', '8.1') );
-      $result->appendChild( $dom->createElement('cgpa', '8.4') );
-
-      //echo '<xmp>'. $dom->saveXML() .'</xmp>';
-      $dom->save('result.xml') or die('XML Create Error');
+      
       ?>
 
    </body>
    </html>
 
    <?php
+   CreatePositions();
 }
 catch (E_EXCEPTION $e) 
 {
@@ -106,50 +95,19 @@ catch (E_EXCEPTION $e)
    DoorTryPage($e);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-/*
-  ?page=1&perpage=20
-
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +="<meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<title>Управление светодиодом</title>\n";
-
-  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
-  ptr +=".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
-  ptr +=".button-on {background-color: #3498db;}\n";
-  ptr +=".button-on:active {background-color: #2980b9;}\n";
-  ptr +=".button-off {background-color: #34495e;}\n";
-  ptr +=".button-off:active {background-color: #2c3e50;}\n";
-  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
-  ptr +="</style>\n";
-  ptr +="</head>\n";
-  ptr +="<body>\n";
-
-  ptr +="<h1>ESP32 Веб сервер</h1>\n";
-    ptr +="<h3>Режим станции (STA)</h3>\n";
-   if(led1stat)
-  {ptr +="<p>Состояние LED1: ВКЛ.</p><a class=\"button button-off\" href=\"/led1off\">ВЫКЛ.</a>\n";}
-  else
-  {ptr +="<p>Состояние LED1: ВЫКЛ.</p><a class=\"button button-on\" href=\"/led1on\">ВКЛ.</a>\n";}
-  if(led2stat)
-  {ptr +="<p>Состояние LED2: ВКЛ.</p><a class=\"button button-off\" href=\"/led2off\">ВЫКЛ.</a>\n";}
-  else
-  {ptr +="<p>Состояние LED2: ВЫКЛ.</p><a class=\"button button-on\" href=\"/led2on\">ВКЛ.</a>\n";}
-  ptr +="</body>\n";
-  ptr +="</html>\n";
-  return ptr;
+// ****************************************************************************
+// *                   Сбросить состояния в Positions.xml                     *
+// ****************************************************************************
+function CreatePositions()
+{
+   $dom = new DOMDocument('1.0','UTF-8');
+   $dom->formatOutput = true;
+   $root = $dom->createElement('positions');
+   $dom->appendChild($root);
+   $root->appendChild( $dom->createElement('name', 'Всем привет!') );
+   $root->appendChild( $dom->createElement('sgpa', '181') );
+   $dom->save('Positions.xml') or die('XML Create Error');
 }
-*/
+
 ?>
 
