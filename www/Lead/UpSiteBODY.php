403f30d9778eeedea894d927759bf4dd97d7895e
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2025-01-01 15:12:30 +0300

    Авторизация в ВК

--- www/Lead/UpSiteBODY.php
+++ www/Lead/UpSiteBODY.php
@@ -2,44 +2,16 @@
 // PHP7/HTML5, EDGE/CHROME/YANDEX                        *** UpSiteBODY.php ***
 
 // ****************************************************************************
-// * KwinFlat                                    Разобрать параметры запроса, *
-// *                                         запустить оболочки страниц сайта *
+// * Lead                                Обеспечить управление контроллерами, *
+// *                                 устройствами и датчиками моего хозяйства *
 // ****************************************************************************
 
-// v2.0, 05.10.2024                                   Автор:      Труфанов В.Е.
+// v2.0.1, 26.12.2024                                 Автор:      Труфанов В.Е.
 // Copyright © 2023 tve                               Дата создания: 08.10.2023
 
 // ------------------------------------------------------------------- BODY ---
 
-echo "Lead!<br>";
-
-/*
-echo '<div id="Left">';
-echo '<div id="Header">';
-*/
-/*
-echo '</div>';
-
-echo '<div id="Article">';
-// Подгружаем обновление значений датчиков, состояний устройств и контроллеров
-require_once("Update/Update.php"); 
-echo '</div>';
-
-echo '<div id="Footer">';
-echo '<pre>';
-echo '$browser='.$browser.'<BR>';
-echo '$version='.$version.'<BR>';
-echo '$platform='.$platform.'<BR>';
-echo '$device_type='.$device_type.'<BR>';
-echo '</pre>';
-echo '</div>';
-echo '</div>';
-
-echo '<div id="Right">';
-echo '$parm='.$parm.'<br>';
-echo '$Page='.$Page.'<br>';
-echo '</div>';
-*/
-
+echo "<Lead>";
+echo "</Lead>";
 
 // <!-- --> ************************************************ UpSiteBODY.php ***
--- www/State/UpSiteBODY.php
+++ www/State/UpSiteBODY.php
@@ -2,44 +2,16 @@
 // PHP7/HTML5, EDGE/CHROME/YANDEX                        *** UpSiteBODY.php ***
 
 // ****************************************************************************
-// * KwinFlat                                    Разобрать параметры запроса, *
-// *                                         запустить оболочки страниц сайта *
+// * State                               Зарегистрировать изменения состояний *
+// *                                        контроллеров и показаний датчиков *
 // ****************************************************************************
 
-// v2.0, 05.10.2024                                   Автор:      Труфанов В.Е.
+// v2.0.1, 26.12.2024                                 Автор:      Труфанов В.Е.
 // Copyright © 2023 tve                               Дата создания: 08.10.2023
 
 // ------------------------------------------------------------------- BODY ---
 
-echo "State!<br>";
-
-/*
-echo '<div id="Left">';
-echo '<div id="Header">';
-*/
-/*
-echo '</div>';
-
-echo '<div id="Article">';
-// Подгружаем обновление значений датчиков, состояний устройств и контроллеров
-require_once("Update/Update.php"); 
-echo '</div>';
-
-echo '<div id="Footer">';
-echo '<pre>';
-echo '$browser='.$browser.'<BR>';
-echo '$version='.$version.'<BR>';
-echo '$platform='.$platform.'<BR>';
-echo '$device_type='.$device_type.'<BR>';
-echo '</pre>';
-echo '</div>';
-echo '</div>';
-
-echo '<div id="Right">';
-echo '$parm='.$parm.'<br>';
-echo '$Page='.$Page.'<br>';
-echo '</div>';
-*/
-
+echo "<State>";
+echo "</State>";
 
 // <!-- --> ************************************************ UpSiteBODY.php ***
