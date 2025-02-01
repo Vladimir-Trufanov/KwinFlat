b50241e05937fedd0b945ef5f3909ca3fe9d2467
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2025-02-01 18:10:38 +0300

    Восстановлено обновление последнего сообщения

--- Kvizzy30/Controller/Controller.js
+++ Kvizzy30/Controller/Controller.js
@@ -29,6 +29,7 @@ $(document).ready(function()
 // ****************************************************************************
 // *               Вызвать jquery-окно для иммитации контроллера              *
 // ****************************************************************************
+var intervalTest1;    // id функции передачи сообщения о смене состояния led33
 function ControllerClick()
 { 
    // По текущему состоянию режима окрашиваем фон элементов управления Led33 
@@ -36,17 +37,65 @@ function ControllerClick()
    console.log("ControllerClick");               
    $('#dial').dialog({
   	   autoOpen: true,
+      beforeClose: function(event,ui) 
+      {
+         // Останавливаем передачю сообщения о смене состояния led33
+         clearInterval(intervalTest1);
+      },
   	   width: 1000
    });
 }
 // ****************************************************************************
-// *               Proba2            *
+// *            Передавать сообщения о смене состояния led33 через 1 сек.     *
 // ****************************************************************************
-function Proba2()
+function SendRequest(url)
+{ 
+   const Http = new XMLHttpRequest();
+   Http.open("GET",url);
+   Http.send();
+   /*
+   Http.onreadystatechange = (e) => 
+   {
+      console.log(Http.responseText); 
+   }
+   */
+}
+function Test1()
+{ 
+   let modeTest1=true;
+   intervalTest1=setInterval(function() 
+   {
+      // console.log("Test1"); 
+      // Поочередно посылаем сообщение, зажигая или гася лампочку 
+      if (modeTest1)
+      {
+         SendRequest('http://localhost:100/State/?cycle=195&sjson={"led33":[{"status":"inHIGH"}]}');
+         modeTest1=false;
+      }
+      else
+      {
+         SendRequest('http://localhost:100/State/?cycle=195&sjson={"led33":[{"status":"inLOW"}]}');
+         modeTest1=true;
+      }
+   }
+   ,1000)
+}
+// ****************************************************************************
+// *               Test2            *
+// ****************************************************************************
+function Test2()
 { 
    // По текущему состоянию режима окрашиваем фон элементов управления Led33 
    // и текст на них 
-   console.log("Proba2");               
+   console.log("Test2");               
 }
-
+// ****************************************************************************
+// *               Test3            *
+// ****************************************************************************
+function Test3()
+{ 
+   // По текущему состоянию режима окрашиваем фон элементов управления Led33 
+   // и текст на них 
+   console.log("Test3");               
+}
 // ********************************************************** Controller.js ***
