7c609b182221dee5cc39dc51ee9acb9e7d09f7b1
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2025-08-23 08:06:28 +0300

    Настроена работа с gpx локально на сайте

--- www/Ex6/index.php
+++ www/Ex6/index.php
@@ -8,11 +8,17 @@
 /**
  * Переработанный пример со страницы GitHub "GPX plugin for Leaflet"
  * (https://github.com/mpetazzoni/leaflet-gpx/tree/main?tab=readme-ov-file)
+ * 
+ * 2025-08-23. Изначально решение не работало в IIS, файл не находился. Пришлось
+ * сделать настройку MIME-типа: в IIS Manager выбрать сайт, дважды нажать на «MIME Types». 
+ * Проверить, указан ли необходимый MIME-тип. Если нет, нажать «Добавить» в панели действий.
+ * Ввести расширение файла и соответствующий MIME-тип (.gpx для text/xml).
+ * Нажать «ОК» для сохранения изменений. Перезагрузить IIS после изменений, 
+ * чтобы все конфигурации были применены.
 **/
  
-// v1.0.6, 22.08.2025                                 Автор:      Труфанов В.Е.
+// v1.0.7, 23.08.2025                                 Автор:      Труфанов В.Е.
 // Copyright © 2025 tve       sla6en9edged            Дата создания: 07.08.2025
-
 
 echo '<!DOCTYPE html>'; // определили разметку HTML5
 echo '<html lang="ru">'; // назначили русский язык для сайта
@@ -33,6 +39,10 @@ echo '<meta http-equiv="content-type" content="text/html; charset=utf-8">';
   }).addTo(map);
 */
 
+/*
+  <script src="js/gpx212.min.js"></script>
+*/
+
 ?>
 <head>
    <title> Мои карты с LeafletJS </title>
@@ -40,7 +50,10 @@ echo '<meta http-equiv="content-type" content="text/html; charset=utf-8">';
 
    <link rel="stylesheet" href="js/leaflet194.css" />
    <script src="js/leaflet194.js"></script>
+   <!--
    <script src="js/gpx212.min.js"></script>
+   -->
+   <script src="js/gpxtve.js"></script>
 
    <style>
       #map { height: 500px; }
@@ -64,9 +77,13 @@ echo '<meta http-equiv="content-type" content="text/html; charset=utf-8">';
    // const url = 'https://mpetazzoni.github.io/leaflet-gpx/demo.gpx';
    // const url = 'https://probatv.ru/Ex5/demo_mpetazzoni.gpx';
    // const url = 'https://probatv.ru/Ex5/mp20230923.gpx';
-   const url = 'https://probatv.ru/Ex5/track20250810.gpx';
+   // const url = 'https://probatv.ru/Ex5/track20250810.gpx';
+   
+   // const url = 'gpx/track20250810.gpx';
+   const url = 'gpx/mp20230923.gpx';
 
-   const options = {
+   const options = 
+   {
       async: true,
       polyline_options: { color: 'red' },
    };
