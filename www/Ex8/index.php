8e4e4dbd28f9bb785a90da8f9deaec8f28f2a203
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2025-09-24 20:08:46 +0300

    Совмещены карты tve и other

--- www/Ex8/index.php
+++ www/Ex8/index.php
@@ -5,13 +5,13 @@
 // *               Ex8. Переключение между картами Osm-Яндекс                 *
 // ****************************************************************************
 
-// v1.0.2, 23.09.2025                                 Автор:      Труфанов В.Е.
-// Copyright © 2025 tve       sla6en9edged            Дата создания: 13.09.2025
+// v1.0.3, 24.09.2025                                 Автор:      Труфанов В.Е.
+// Copyright © 2025 tve                               Дата создания: 13.09.2025
 
 define ("tve",    "tve"); 
 define ("guest",  "гость"); 
 define ("nearby", "близкие"); 
-define ("permit", tve); 
+define ("permit", guest); 
 
 ?>
 <html>
@@ -30,9 +30,25 @@ define ("permit", tve);
 
 <div style="width:100%; height:100%" id="map"></div>
 
-<script>
-	function MakeMapTVE(nlat,nlon,nzoom) 
-  {
+<?php
+
+$lat=61.846308; $lon=33.206584; $zoom=10;  // Центр в Эссойле
+
+// Строим Яндекс и OSM карты 
+if (permit==tve) MakeMapTVE($zoom,$lat,$lon); 
+// Строим только OSM карту 
+else MakeMapOther($zoom,$lat,$lon); 
+
+// ****************************************************************************
+// *        Построить Яндекс и OSM-карты с возможностью переключения          *
+// *   с центром в текущей точке геолокации (если геолокация разрешена) или   *
+// *             в заданной точке (по умолчанию - Петрозаводск)               *
+// ****************************************************************************
+function MakeMapTVE($nzoom,$nlat=61.8021,$nlon=34.3296) 
+{
+  echo "<script> var nlat=".$nlat.",nlon=".$nlon.",nzoom=".$nzoom."; </script>"; 
+  ?>
+  <script>
 	  let center=[nlat,nlon];
 	  let map = L.map('map', 
     {
@@ -70,187 +86,80 @@ define ("permit", tve);
 		  let actualProvider = new ymaps.traffic.provider.Actual({}, { infoLayerShown: true });
 		  actualProvider.setMap(this._yandex);
 	  }
-  }
   </script>
+  <?php
+}
 
-<?php
-echo "
-<script>
-  var nlat,nlon,nzoom; 
-  nlat=67.6755; nlon=33.936; nzoom=10;
-  MakeMapTVE(nlat,nlon,nzoom);
-</script>
-";
-
-
-
-/*
-echo "
-<script>
-  / *
-     var nlatin,nlonin,incenter;
-     if (navigator.geolocation) 
-     {
-        navigator.geolocation.getCurrentPosition(
-        function (position) 
-        {
-           nlatin = position.coords.latitude;
-           nlonin = position.coords.longitude;
-           accuracy = position.coords.accuracy; // Погрешность в метрах
-           console.log('1. nlat='+nlatin,'nlon='+nlonin);
-	         incenter=[nlatin,nlonin];
-           return incenter;
-        }, 
-        function () 
-        {
-           alert(\"Не удалось получить ваше местоположение.\");
-        });
-     } 
-     else 
-     {
-        alert(\"Ваш браузер не поддерживает геолокацию.\");
-     }
-
-
-
-
-
-  var nlat,nlon,nzoom,accuracy; 
-  nlat=67.6755;
-  nlon=33.936;
-  nzoom=10;
-  accuracy=0;
-  
-	var center=[nlat,nlon];
-	//var center=getlocation();
-  console.log('3. nlat='+nlat,'nlon='+nlon);
-	var map = L.map('map', 
-  {
-		center:center,
-		zoom:nzoom,
-		zoomAnimation:true
-	});
-  // Готовим маркер 'флаг трека'
-  var DirFlagIconOptions = 
-  {
-    iconUrl: '../gpx/js/images/DirFlag47x47.png',
-    iconSize: [47,47]
-  }
-  var DirFlagIcon = L.icon(DirFlagIconOptions);
-  var DirFlagMarkerOptions = 
-  {
-    title: 'Мы здесь',
-    clickable: false,
-    draggable: false,
-    icon: DirFlagIcon
-  }
-  // Устанавливаем и подписываем маркер
-  var DirFlagMarker = L.marker(center,DirFlagMarkerOptions);
-  DirFlagMarker.addTo(map);
-  * /
-  
-  / *
-  function getlocation() 
-  {
-     var nlatin,nlonin,incenter;
-     if (navigator.geolocation) 
-     {
-        navigator.geolocation.getCurrentPosition(
-        function (position) 
-        {
-           nlatin = position.coords.latitude;
-           nlonin = position.coords.longitude;
-           accuracy = position.coords.accuracy; // Погрешность в метрах
-           console.log('1. nlat='+nlatin,'nlon='+nlonin);
-	         incenter=[nlatin,nlonin];
-           return incenter;
-        }, 
-        function () 
-        {
-           alert(\"Не удалось получить ваше местоположение.\");
-        });
-     } 
-     else 
-     {
-        alert(\"Ваш браузер не поддерживает геолокацию.\");
-     }
-     console.log('2. nlat='+nlatin,'nlon='+nlonin);
-	   incenter=[nlatin,nlonin];
-     return incenter;
-  }
-  * /
-</script>
-";
-
-if (permit==tve)
+// ****************************************************************************
+// *        Построить Яндекс и OSM-карты с возможностью переключения          *
+// *   с центром в текущей точке геолокации (если геолокация разрешена) или   *
+// *             в заданной точке (по умолчанию - Петрозаводск)               *
+// ****************************************************************************
+function MakeMapOther($nzoom,$nlat=61.8021,$nlon=34.3296) 
 {
-echo "
-<script>
-  MakeMapTVE();
-	function MakeMapTVE() 
+  echo "<script> var nlat=".$nlat.",nlon=".$nlon.",nzoom=".$nzoom."; </script>"; 
+  ?>
+  <script>
+
+  if (navigator.geolocation) 
   {
-  
-    var baseLayers = 
-    {
-		  'OSM':               L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
-      {
-			  attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'
-		  }).addTo(map),
-		  'Yandex map':        L.yandex(),           
-		  'Yandex satellite':  L.yandex({type:'satellite'}),  // тип может быть указан опционально
-		  'Yandex hybrid':     L.yandex('hybrid')
-	  };
+     navigator.geolocation.getCurrentPosition(
+     function (position) 
+     {
+       var nlatin = position.coords.latitude;
+       var nlonin = position.coords.longitude;
+       var accuracy = position.coords.accuracy; // Погрешность в метрах
+       MakeMap(nlatin,nlonin,nzoom); 
+     }, 
+     function () 
+     {
+       alert("Не удалось получить ваше местоположение.");
+       MakeMap(nlat,nlon,nzoom); 
+     });
+  } 
+  else 
+  {
+     alert("Ваш браузер не поддерживает геолокацию.");
+     MakeMap(nlat,nlon,nzoom) 
+  }
 
-	  var overlays = 
-    {
-	  	'Traffic':L.yandex('overlay').on('load', traffic)
-    };
-
-	  var center=[67.6755,33.936];
-    console.log('3. nlat='+nlat,'nlon='+nlon);
+  function MakeMap(nlat,nlon,nzoom,accuracy) 
+  {
+    // Готовим пространство под карту с центром и зумом
+	  var center=[nlat,nlon];
 	  var map = L.map('map', 
     {
 		  center:center,
 		  zoom:nzoom,
 		  zoomAnimation:true
 	  });
-	  map.attributionControl.setPosition('bottomleft').setPrefix('');
-
-	  L.control.layers(baseLayers, overlays, {collapsed:true}).addTo(map);
-	  var marker = L.marker(center, {draggable:false}).addTo(map);
-  	map.locate({setView:true, maxZoom:19})
-		.on('locationfound',function (e) 
+    // Загружаем OpenStreetMap
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
     {
-			marker.setLatLng(e.latlng);
-		});
+      attribution: 'Map data © OpenStreetMap contributors',
+      maxZoom: 19,
+    }).addTo(map);
+    // Готовим маркер 'флаг трека'
+    var DirFlagIconOptions = 
+    {
+      iconUrl: '../gpx/js/images/DirFlag47x47.png',
+      iconSize: [47,47]
+    }
+    var DirFlagIcon = L.icon(DirFlagIconOptions);
+    var DirFlagMarkerOptions = 
+    {
+      title: 'Мы здесь',
+      clickable: false,
+      draggable: false,
+      icon: DirFlagIcon
+    }
+    // Устанавливаем и подписываем маркер
+    var DirFlagMarker = L.marker(center,DirFlagMarkerOptions);
+    DirFlagMarker.addTo(map);
   }
-
-	function traffic () 
-  {
-		// https://tech.yandex.ru/maps/jsbox/2.1/traffic_provider
-		var actualProvider = new ymaps.traffic.provider.Actual({}, { infoLayerShown: true });
-		actualProvider.setMap(this._yandex);
-	}
-</script>
-";
+  </script>
+  <?php
 }
-
-
-else
-{
-/ *
-echo "
-<script>
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
-  {
-    attribution: 'Map data © OpenStreetMap contributors',
-    maxZoom: 19,
-  }).addTo(map);
-</script>
-";
-* /
-}
-*/
 
 ?>
 </body>
