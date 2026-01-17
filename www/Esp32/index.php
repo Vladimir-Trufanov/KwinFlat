61ff720903ddbe4881f38043aaeb3c0b6103c72e
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2026-01-17 13:35:53 +0300

    PHP заставил установить каталог vendor из Probatv

---
+++ www/Esp32/index.php
@@ -0,0 +1,37 @@
+<?php
+// PHP7/HTML5, EDGE/CHROME/YANDEX                             *** index.php ***
+
+// ****************************************************************************
+// * Esp32                        Тестовые примеры для взаимодействия с Esp32 *
+// ****************************************************************************
+
+/**
+ * 
+**/
+ 
+// v1.0.0, 05.01.2026                                 Автор:      Труфанов В.Е.
+// Copyright © 2026 tve                               Дата создания: 05.01.2026
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Тестовые примеры для взаимодействия с Esp32</title>
+   <link rel="stylesheet" href="leaflet194/leaflet.css" />
+   <style>
+      #map {height: 500px;}
+   </style>
+</head>
+
+<body>
+  <div id="map">
+  Privet_ИК!
+  </div>
+</body>
+</html>
+<?php
+
+?> <!-- --> <?php // ******************************************** index.php ***
--- www/index.php
+++ www/index.php
@@ -1,4 +1,5 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
 // PHP7/HTML5, EDGE/CHROME                                    *** index.php ***
 
 // ****************************************************************************
